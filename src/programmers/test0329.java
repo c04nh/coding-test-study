d1df836299ec80a8ec81592c8ab24df26ec3116c
Nahyun Cho <w2038@e-mirim.hs.kr>
2022-03-29 09:05:24 +0900

    [ADD] 0329 x만큼 간격이 있는 n개의 숫자 (#6)

---
+++ src/programmers/test0329.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class test0329 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        for(int i = 0; i < n; i++){
+            answer[i] = num;
+            num += x;
+        }
+        return answer;
+    }
+}
