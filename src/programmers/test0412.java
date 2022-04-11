9812ad0d29ce4ae1c7291f6df3ae73ff9078aa6b
Nahyun Cho <w2038@e-mirim.hs.kr>
2022-04-12 08:37:36 +0900

    [ADD] 0412 자연수 뒤집어 배열로 만들기

---
+++ src/programmers/test0412.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class test0412 {
+    public int[] solution(long n) {
+        int[] answer = new int[String.valueOf(n).length()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = Long.valueOf(n % 10).intValue();
+            n /= 10;
+        }
+        return answer;
+    }
+}
