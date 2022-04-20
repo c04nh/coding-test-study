c0873c17b4fdf436614847030386ba2c3fd59d72
Nahyun Cho <nhsally@naver.com>
2022-04-21 08:24:27 +0900

    [ADD] 0421 서울에서 김서방 찾기

---
+++ src/programmers/test0421.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class test0421 {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+        return answer;
+    }
+}
