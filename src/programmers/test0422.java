70b5178f1752182c62da2b5364f978056db7f7ab
nhsally <nhsally@naver.com>
2022-04-22 08:49:52 +0900

    [ADD] 0422 전화번호 목록

---
+++ src/programmers/test0422.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class test0422 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++){
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
+        return answer;
+    }
+}
