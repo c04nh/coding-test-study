8d0e1edda0049d787833bc2e656bea5fa0c7b52c
Nahyun Cho <w2038@e-mirim.hs.kr>
2022-05-18 18:27:07 +0900

    [0518] 같은 숫자는 싫어

---
+++ src/programmers/test0518.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class test0518 {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList();
+        int saveNum = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(saveNum != arr[i]){
+                saveNum = arr[i];
+                list.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
